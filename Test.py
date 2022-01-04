30cd7336eff00d635d17183d7270ffc965418808
Emre <emrecpp1@gmail.com>
2022-01-04 15:50:37 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,20 +1,20 @@
 # Packet Handler Test by Emre Demircan (https://www.github.com/emrecpp) (https://www.github.com/emrecpp/PacketHandler)
-# 2021-04-02 | v1.0.7
+# 2022-01-04 | v1.0.8
 from Packet import Packet, ref
 import sys, socket, select, time
 from threading import Thread
 
 class opcodes:
-    LOGIN=100
-    LOGOUT=101
+    LOGIN  = 100
+    LOGOUT = 101
 
-HOST, PORT = "127.0.0.1", 2000
-
+HOST, PORT = "127.0.0.1", 2022
 
 # *** SERVER ***
 def Server():
     sServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
     sServer.bind((HOST, PORT))
     sServer.listen(10)
     read_list, outputs=[sServer], []
@@ -23,10 +23,10 @@ def Server():
         for s in readable:
             if s is sServer: # Got new connection
                 ClientSocket, addr = sServer.accept()
-                ClientSocket.setblocking(0)
+                ClientSocket.setblocking(1)
                 read_list.append(ClientSocket)
             else: # Got new data from already connected socket
-                PacketListen = Packet()
+                PacketListen = Packet(PrintErrorLog=True, Compress=True, Encrypt=True, LittleEndian=True)
                 if PacketListen.Recv(s): # Will be automatically Decrypted / UnCompressed if Encrypted or Compressed
                     PacketListen.Print("RECEIVED PACKET (YOUR TITLE)!")
                     if PacketListen.GetOpcode() == opcodes.LOGIN:
@@ -36,8 +36,10 @@ def Server():
                         # Note: Can't use bool(RememberMe), this returns True everytime!!!
 
                         print(f"Username: {UserName}\nPassword: {Password}\nRememberMe: {'Yes' if RememberMe else 'No'}\nData: {str(Data)}\nFruits: {Fruits}")
-
+                    elif PacketListen.GetOpcode() == opcodes.LOGOUT:
+                        pass
                 else:
+                    print("Socket number: %d closed." % (ClientSocket.fileno()))
                     if ClientSocket in read_list: read_list.remove(ClientSocket) # Remove from read list if socket closed
 
 
@@ -45,19 +47,18 @@ def Server():
 def Client():
     sClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sClient.connect((HOST, PORT))
-
-    Paket = Packet(opcodes.LOGIN, Encrypt=True, Compress=True)
+    Paket = Packet(opcodes.LOGIN, Encrypt=True, Compress=True, LittleEndian=True)
     Username = "Emre"
 
     Paket << Username << "123" << True << bytearray(b'\x07\x10BYTES\xFF') << ["Apple", "Banana", "Orange"]
-    Paket.Send(sClient)
+    ResultSend = Paket.Send(sClient)
+    print("Sent [C->S]: %s" % ("Success" if ResultSend else "Fail"))
 
 
 if __name__ == "__main__":
-    thrServer = Thread(target=Server, args=(), name="THR_SERVER")
-    thrServer.daemon=True
+    thrServer = Thread(target=Server, args=(), name="THR_SERVER", daemon=True)
     thrServer.start()
-
     Client()
-    time.sleep(2000)
+    time.sleep(5)
+    sys.exit(0)
 
